af46a0ea3ca96397c6d6b6014eeeb086bb6d24e0
Muhammad Zuhri Hanifullah <zuhri2395@gmail.com>
2017-05-13 19:42:43 +0700

    add file upload form

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>FP TREE</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <form action="proses.php" enctype="multipart/form-data">
+            <input type="file" name="data">
+            <button type="submit">PROSES</button>
+        </form>
+    </body>
+</html>
